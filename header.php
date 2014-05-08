0dd3b9ebe03fde11a263b9b559b00b6d5c3ab57a
lorzkwan <lorzkwan@gmail.com>
2014-05-08 11:32:08 +0800

    using html5 doctype

--- header.php
+++ header.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head profile="http://gmpg.org/xfn/11">
-
-
-
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
 	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
-
-
 
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />	
 
@@ -33,9 +27,7 @@
 	<?php //comments_popup_script(); // off by default ?>
 
 	<?php wp_head(); ?>
-
 </head>
-
 <body>
 	<div id="wrapper">
 
@@ -47,5 +39,3 @@
 			</h1>
 			<?php bloginfo('description');?>
 		</div>
-
-
